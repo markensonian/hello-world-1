f55c08e9ca1975dc4e6bf20eb7964446f43e5daf
Markenson Delkhaste <mjdelkhaste@gmail.com>
2023-06-28 19:11:37 -0700

    trial

---
+++ app.js
@@ -0,0 +1,10 @@
+// Anatomy of a function
+// Function declaration
+// function nameOfFunction(parameters){
+//    code to be executed; 
+//    code to be executed;
+// }
+
+function starWarsGames(){
+    
+} 
